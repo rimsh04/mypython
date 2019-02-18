0091197023151677fd91a5669e4aa3297b66c808
rimsh04 <rimsha.mariam17@gmail.com>
2019-02-18 23:10:11 +0530

    Chapter3

---
+++ exc3.py
@@ -0,0 +1,19 @@
+print('enter your score')
+try:
+ score=float(input())
+ if(score<0.0 or score>1.0):
+    print('Out of range')
+ else:
+    if(score>=0.9):
+        print('A')
+    elif(score>=0.8):
+        print('B')
+    elif(score>=0.7):
+        print('C')
+    elif(score>=0.6):
+        print('D')
+    else:
+        print('F')
+except:
+    print('Please enter a numeric value')
+
