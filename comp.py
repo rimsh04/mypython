de83b634e5ce070fd0ec03b03d302bcd1d5d2112
rimsh04 <rimsha.mariam17@gmail.com>
2019-02-20 10:52:33 +0530

    compare 3

---
+++ comp.py
@@ -0,0 +1,10 @@
+def compare():
+    a=10
+    b=2
+    c=7
+    if((a>b) and (a>c)):
+        print('a is greatest')
+    if((b>a) and (b>c)):
+        print('b is greatest')
+    if((c>a) and (c>b)):
+        print('c is greatest')
---
+++ fibonacci.py
@@ -0,0 +1,8 @@
+def fibo():
+    a=0
+    b=1
+    while True:
+        yield a
+        a=b
+        b=a+b
+print (list(fibo()))
